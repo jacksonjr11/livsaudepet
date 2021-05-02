c9b9f56631dbfd377e1dd229cea36429597bf556
jacksonjr11 <jackson.augusto.1571@gmail.com>
2021-05-02 12:01:41 -0300

    Atualização

--- src/mock/contatos.ts
+++ src/mock/contatos.ts
@@ -2,26 +2,26 @@ export const contatos = [
   {
     type: 'W',
     contact: '(85) 98844-3168',
-    link: 'https://api.whatsapp.com/send?phone=5585988443168&text=Ol%C3%A1%2C%20gostaria%20de%20agendar%20a%20minha%20instala%C3%A7%C3%A3o.',
+    link: '',
   },
   {
     type: 'W',
     contact: '(85) 98886-8653',
-    link: 'https://api.whatsapp.com/send?phone=5585988868653&text=Ol%C3%A1%2C%20gostaria%20de%20saber%20sobre%20planos%20e%20cobertura.',
+    link: '',
   },
   {
     type: 'E',
-    contact: 'linkwantelecom@gmail.com',
-    link: 'mailto://linkwantelecom@gmail.com'
+    contact: 'livsaudepet@gmail.com',
+    link: ''
   },
   {
     type: 'I',
     contact: 'Instagram',
-    link: 'https://www.instagram.com/linkwantelecom/'
+    link: ''
   },
   {
     type: 'F',
     contact: 'Facebook',
-    link: 'https://www.facebook.com/linkwantelecom/'
+    link: ''
   }
 ]
