d890464826bbe79f2f8e3189029a66450ecc205e
jacksonjr11 <jackson.augusto.1571@gmail.com>
2021-05-03 22:12:45 -0300

    finish

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import networkImg from "../../assets/vectors/network.png";
 import "./styles.scss";
+import dogcat from "../../assets/vectors/dogcat.jpg"
 
 const HomeSection: React.FC = () => {
   const handleMoveToContacts = React.useCallback(() => {
@@ -14,6 +15,7 @@ const HomeSection: React.FC = () => {
     <section id="Home">
       <div className="aboutleft">
         <section>
+          <img src={dogcat} alt="dogcat" className="dogcat"/>
           <h1 className="title">Liv Saúde Pet</h1>
           <h3 className="slogan">Seu Pet com mais saúde.</h3>
         </section>
